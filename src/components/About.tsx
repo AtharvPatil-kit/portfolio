0a73bfdf1445a059880c3105b8c2aa77ee4f25e5
Atharv Patil <atharvapatil808@gmail.com>
2026-08-28 01:24:10 +0530

    Initial commit

---
+++ src/components/About.tsx
@@ -0,0 +1,20 @@
+import "./styles/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-me">
+        <h3 className="title">About Me</h3>
+        <p className="para">
+          Aspiring engineer currently pursuing undergraduate studies, driven by curiosity and a desire to build impactful solutions. Eager to explore emerging technologies, enhance technical expertise, and gain hands‑on experience through projects and collaborative environments.
+          applications using React.js, Angular, Next.js, Node.js, and NestJS. Skilled
+          in microservices architecture, CMS development, and low-code platforms.
+          Passionate about creating high-performance, production-ready solutions
+          from concept to deployment.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
