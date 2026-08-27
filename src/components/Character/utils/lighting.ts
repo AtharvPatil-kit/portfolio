23cf25559396a3a3fea5c7182c560e469057671c
Atharv Patil <atharvapatil808@gmail.com>
2026-08-28 02:10:55 +0530

    Fix HDR environment path

--- src/components/Character/utils/lighting.ts
+++ src/components/Character/utils/lighting.ts
@@ -18,9 +18,9 @@ const setLighting = (scene: THREE.Scene) => {
   pointLight.castShadow = true;
   scene.add(pointLight);
 
-  new RGBELoader()
-    .setPath("models/")
-    .load("char_enviorment.hdr?v=2", function (texture) {
+new RGBELoader()
+  .setPath("/models/")
+  .load("char_enviorment.hdr?v=2", function (texture) {
       texture.mapping = THREE.EquirectangularReflectionMapping;
       scene.environment = texture;
       scene.environmentIntensity = 0;
